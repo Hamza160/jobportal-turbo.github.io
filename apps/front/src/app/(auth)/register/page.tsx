a9fb1b718231fcdc92865d6a88d0212b1140436f
Hamza Ashraf <hamzaashraf160@gmail.com>
2026-01-06 11:05:44 +0500

    Working on register form

--- apps/front/src/app/(auth)/register/page.tsx
+++ apps/front/src/app/(auth)/register/page.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import FormInput from "@/components/FormInput";
 
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <form>
+        <h1 className="font-bold text-2xl mb-4 text-yellow-400 text-center">Sign Up</h1>
+        <FormInput
+            label={'Full Name'}
+            type={'text'}
+            name={'fullName'}
+            placeholder={'Enter your full name'}
+        /><FormInput
+            label={'Email'}
+            type={'email'}
+            name={'email'}
+            placeholder={'Enter your email'}
+        /><FormInput
+            label={'Phone Number'}
+            type={'text'}
+            name={'phoneNmber'}
+            placeholder={'Enter your full name'}
+        /><FormInput
+            label={'Full Name'}
+            type={'text'}
+            name={'fullName'}
+            placeholder={'Enter your full name'}
+        />
+    </form>
   );
 }
 
---
+++ apps/front/src/components/FormInput.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+
+type Props = {
+    label: string;
+    type: string;
+    name: string;
+    value?: string;
+    placeholder: string;
+    onChange?: () => void;
+    defaultValue?: string;
+
+}
+
+const FormInput = ({label, type, value, onChange, name, placeholder, defaultValue}: Props) => {
+    return (
+        <div className="my-1">
+            <Label htmlFor={name}>{label}</Label>
+            <Input
+                id={name}
+                type={type}
+                placeholder={placeholder}
+                defaultValue={defaultValue}
+                value={value}
+                onChange={onChange}
+                className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+            />
+        </div>
+    );
+}
+
+export default FormInput;
